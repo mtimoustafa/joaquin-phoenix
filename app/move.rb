5eef01b8ab5b18b003a06385cb54e740c50acd3a
Miko Taruc <mikhael.taruc@vidyard.com>
2019-11-28 12:35:54 -0500

    avoidant

--- app/move.rb
+++ app/move.rb
@@ -1,26 +1,59 @@
-# TODO: Implement your logic here!
-# View docs at https://docs.battlesnake.com/snake-api for example payloads.
-def move(board)
-  pp "Wtf"
-  height = board[:board][:height]
-  width = board[:board][:width]
-  myBody = board[:you][:body]
-  currentHead = myBody[0]
-  currentHealth = board[:you][:health]
+$potential = { :up => 0, :right => 0, :down => 0, :left => 0 }
 
-  if currentHead[:y] > 0
-    return { move: :up }
-  end
-
-  if currentHead[:x] > 0
-    return {move: :left}
-  end
-
+def checkWall(currentHead, width, height)
   if currentHead[:y] === 0
-    return { move: :down }
+    $potental[:up] -= 5
   end
 
   if currentHead[:x] === 0
-    return {move: :right}
+    $potential[:left] -= 5
   end
+
+  if currentHead[:y] === height
+    $potential[:down] -= 5
+  end
+
+  if currentHead[:x] === width
+    $potential[:right] -= 5
+  end
+end
+
+def checkMySnake(currentHead, myBody)
+  goRight = { :x => currentHead[:x] + 1, :y => currentHead[:y] }
+  goLeft = { :x => currentHead[:x] - 1, :y => currentHead[:y] }
+  goUp = { :x => currentHead[:x], :y => currentHead[:y] - 1 }
+  goDown = { :x => currentHead[:x], :y => currentHead[:y] + 1 }
+
+  if myBody.include?(goRight)
+    $potential[:right] -= 5
+  end
+
+  if myBody.include?(goLeft)
+    $potential[:left] -= 5
+  end
+
+  if myBody.include?(goDown)
+    $potential[:down] -= 5
+  end
+
+  if myBody.include?(goUp)
+    $potential[:up] -= 5
+  end
+end
+
+def move(board)
+  myBody = board[:you][:body]
+  currentHead = myBody[0]
+  width = board[:board][:width]
+  height = board[:board][:height]
+  currentHealth = board[:you][:health]
+
+  # Avoidant logic
+  checkWall(currentHead, width, height)
+  checkMySnake()
+  # checkOtherSnake
+
+  # checkFood
+  # checkLethality
+  return { move: :up }
 end
