a3a9d9b9acda08fd89546162f7514d12a2d3218a
Miko Taruc <mikhael.taruc@vidyard.com>
2019-11-28 14:29:37 -0500

    fix x y

--- app/helpers/checkOtherSnakes.rb
+++ app/helpers/checkOtherSnakes.rb
@@ -1,23 +1,23 @@
 def checkOtherSnakes(currentHead, enemySnakes)
-  goRight = { :x => currentHead[:x] + 1, :y => currentHead[:y] }
-  goLeft = { :x => currentHead[:x] - 1, :y => currentHead[:y] }
-  goUp = { :x => currentHead[:x], :y => currentHead[:y] - 1 }
-  goDown = { :x => currentHead[:x], :y => currentHead[:y] + 1 }
+  goDown = { :x => currentHead[:x] + 1, :y => currentHead[:y] }
+  goUp = { :x => currentHead[:x] - 1, :y => currentHead[:y] }
+  goLeft = { :x => currentHead[:x], :y => currentHead[:y] - 1 }
+  goRight = { :x => currentHead[:x], :y => currentHead[:y] + 1 }
 
   enemySnakes.each do |snake|
-    if snake.include?(goRight)
+    if snake[:body].include?(goRight)
       $potential[:right] -= 5
     end
 
-    if snake.include?(goLeft)
+    if snake[:body].include?(goLeft)
       $potential[:left] -= 5
     end
 
-    if snake.include?(goDown)
+    if snake[:body].include?(goDown)
       $potential[:down] -= 5
     end
 
-    if snake.include?(goUp)
+    if snake[:body].include?(goUp)
       $potential[:up] -= 5
     end
   end
--- app/helpers/wall.rb
+++ app/helpers/wall.rb
@@ -1,17 +1,17 @@
 def checkWall(currentHead, width, height)
   if currentHead[:y] === 0
-    $potential[:up] -= 5
-  end
-
-  if currentHead[:x] === 0
     $potential[:left] -= 5
   end
 
+  if currentHead[:x] === 0
+    $potential[:up] -= 5
+  end
+
   if currentHead[:y] === height - 1
-    $potential[:down] -= 5
+    $potential[:right] -= 5
   end
 
   if currentHead[:x] === width - 1
-    $potential[:right] -= 5
+    $potential[:down] -= 5
   end
 end
