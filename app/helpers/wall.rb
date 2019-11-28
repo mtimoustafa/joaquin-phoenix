00ebf3672be5452b0c87ad92aa611e9caa32fffb
Miko Taruc <mikhael.taruc@vidyard.com>
2019-11-28 14:40:46 -0500

    woops

--- app/helpers/wall.rb
+++ app/helpers/wall.rb
@@ -1,10 +1,10 @@
 def checkWall(currentHead, width, height)
   if currentHead[:y] === 0
-    $potential[:up] -= 5
+    $potential[:left] -= 5
   end
 
   if currentHead[:x] === 0
-    $potential[:left] -= 5
+    $potential[:up] -= 5
   end
 
   if currentHead[:y] === height - 1
