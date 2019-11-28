eb47682bc7bc0d08feeef07539ce21c22836a879
mtimoustafa <mohamed.moustafa@vidyard.com>
2019-11-28 14:39:41 -0500

    put globals back

--- app/move.rb
+++ app/move.rb
@@ -1,6 +1,8 @@
 require './app/helpers/wall'
 require './app/helpers/checkMySnake'
 
+$potential = { :up => 0, :right => 0, :down => 0, :left => 0 }
+
 def move(board, debug = false)
   debug_log = { board: board }
 
@@ -25,6 +27,8 @@ def move(board, debug = false)
       direction = key
     end
   }
+  
+  $potential = { :up => 0, :right => 0, :down => 0, :left => 0 }
 
   debug_log = { debug: debug_log }
   return {move: direction}.merge(debug_log)
