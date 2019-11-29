a9e1b7637e6a9aac44128809f4eed7fa89cb98c2
Miko Taruc <mikhael.taruc@vidyard.com>
2019-11-29 10:20:27 -0500

    avoid outer wall

--- app/helpers/wall.rb
+++ app/helpers/wall.rb
@@ -14,4 +14,20 @@ def checkWall(currentHead, width, height)
   if currentHead[:y] === height - 1
     $potential[:down] -= 10
   end
+
+  if currentHead[:x] === 1
+    $potential[:left] -= 1
+  end
+
+  if currentHead[:y] === 1
+    $potential[:up] -= 1
+  end
+
+  if currentHead[:x] === width - 2
+    $potential[:right] -= 1
+  end
+
+  if currentHead[:y] === height - 2
+    $potential[:down] -= 1
+  end
 end
